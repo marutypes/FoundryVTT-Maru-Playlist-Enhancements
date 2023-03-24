30042c3906ed663e3c79a43ab4a5dc1b1f9d4c66
Mallory Allen <mallorytypes@gmail.com>
2023-03-24 17:03:34 -0400

    :bug: fix localization issues

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,22 +1,27 @@
-Hooks.on('init', () => {
-    Hooks.on('renderPlaylistDirectory', (app, html, data) => {
-      const playExclusiveButton = $('<button class="play-exclusively"><i class="fas fa-circle-play"></i> Play Exclusively</button>');
-  
-      playExclusiveButton.click(async (event) => {
-        event.preventDefault();
-        
-        const playlistId = event.currentTarget.closest('.playlist').dataset.playlistId;
-        const playlists = game.playlists.entities;
-  
-        for (let playlist of playlists) {
-          if (playlist.id === playlistId) {
-            await playlist.playAll();
-          } else {
-            await playlist.stopAll();
-          }
+Hooks.on("init", () => {
+  Hooks.on("renderPlaylistDirectory", (app, html, data) => {
+    const playExclusiveButton = $(
+      `<button class="play-exclusively"><i class="fas fa-play"></i> ${game.i18n.localize(
+        "PLAY_EXCLUSIVELY"
+      )}</button>`
+    );
+
+    playExclusiveButton.click(async (event) => {
+      event.preventDefault();
+
+      const playlistId =
+        event.currentTarget.closest(".playlist").dataset.playlistId;
+      const playlists = game.playlists.entities;
+
+      for (let playlist of playlists) {
+        if (playlist.id === playlistId) {
+          await playlist.playAll();
+        } else {
+          await playlist.stopAll();
         }
-      });
-  
-      html.find('.playlist-controls').append(playExclusiveButton);
+      }
     });
+
+    html.find(".playlist-controls").append(playExclusiveButton);
   });
+});
