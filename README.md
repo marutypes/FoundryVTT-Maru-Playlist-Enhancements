5e8b590218f6b9db93a27f79653352c4278966dc
Mallory Allen <mallorytypes@gmail.com>
2023-03-24 18:39:46 -0400

    :sparkles: add stop all button

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 This FoundryVTT module adds some enhancements to Foundry's playlists:
 
 - Adds a "Play Exclusively" button to playlists in the playlist directory. When clicked, the selected playlist will play, and all other playlists will stop.
+- Adds a "Stop All" button to the Currently Playing drawer, allowing you to easily clear up leftover sounds from a previous scene
--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,27 +1,74 @@
 Hooks.on("init", () => {
-  Hooks.on("renderPlaylistDirectory", (app, html, data) => {
-    const playExclusiveButton = $(
-      `<button class="play-exclusively"><i class="fas fa-play"></i> ${game.i18n.localize(
-        "PLAY_EXCLUSIVELY"
-      )}</button>`
-    );
+  Hooks.on("renderPlaylistDirectory", (_app, html, _data) => {
+    addPlayExclusiveButton(html);
+    addStopAllButton(html);
+  });
+  log("renderPlaylistDirectory modifications hooked");
+});
 
-    playExclusiveButton.click(async (event) => {
-      event.preventDefault();
+function addPlayExclusiveButton(html) {
+  const playExclusiveButton = SoundControl({
+    title: game.i18n.localize("PLAY_EXCLUSIVELY"),
+    icon: "play-circle",
+  }).click(async (event) => {
+    event.preventDefault();
 
-      const playlistId =
-        event.currentTarget.closest(".playlist").dataset.playlistId;
-      const playlists = game.playlists.entities;
+    const playlistId =
+      event.currentTarget.closest(".playlist").dataset.documentId;
+    const { playlists } = game;
 
-      for (let playlist of playlists) {
-        if (playlist.id === playlistId) {
-          await playlist.playAll();
-        } else {
+    for (let playlist of playlists) {
+      if (playlist.id === playlistId) {
+        await playlist.playAll();
+      } else {
+        await playlist.stopAll();
+      }
+    }
+  });
+
+  html.find(".playlist-controls").append(playExclusiveButton);
+}
+
+function addStopAllButton(html) {
+  const playingDrawer = html.find("#currently-playing");
+
+  if (playingDrawer.length === 0) {
+    return;
+  }
+  let stopAllButton = playingDrawer.find(".stop-all");
+  if (stopAllButton.length === 0) {
+    const stopAllButton = SoundControl({
+      title: game.i18n.localize("STOP_ALL"),
+      icon: "circle-stop",
+    })
+      .click(async (event) => {
+        event.preventDefault();
+
+        const { playlists } = game;
+
+        for (let playlist of playlists) {
           await playlist.stopAll();
         }
-      }
-    });
+      })
+      .css({
+        "justify-self": "end",
+        "justify-content": "end",
+        "align-self": "center",
+        flex: "0",
+        "padding-right": "10px",
+      });
+    playingDrawer.find(".playlist-header").append(stopAllButton);
+  }
+}
 
-    html.find(".playlist-controls").append(playExclusiveButton);
-  });
-});
+function SoundControl(props = {}) {
+  const { title, icon } = props;
+  const control = $(
+    `<a class="sound-control"><i class="fas fa-${icon}" title="${title}"></i></a>`
+  );
+  return control;
+}
+
+function log(msg) {
+  console.log("Maru's Playlist Enhancements |", msg);
+}
